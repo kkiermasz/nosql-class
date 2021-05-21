9f7638448fb235b2924932db2c181bee1f76621b
Jakub Kiermasz <kiermasz.k@gmail.com>
2021-05-21 15:20:42 +0200

    Finish

--- index.js
+++ index.js
@@ -1,23 +1,86 @@
 'use strict';
 
 var express = require('express');
-var bodyParser = require('body-parser');
+var Realm = require('realm');
 var app = express();
 
 app.use(express.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send("Hello Realm");
+  let databases = realm.objects('DB').sorted('name', true);
+  res.render('pages/index', {databases: databases});
 });
 
 app.listen(3000, function() {
   console.log("Go!");
 });
 
-app.get('/add', function(req, res) {
-  res.sendFile(__dirname + "/add.html");
+app.get('/add_db', function(req, res) {
+  let owners = realm.objects('Owner').sorted('name', true);
+  res.render('pages/add_db', {owners: owners});
 });
 
-app.post('/add', function(req, res) {
-  res.send(req.body);
+app.get('/add_owner', function(req, res) {
+  res.render('pages/add_owner');
+});
+
+app.get('/owners', function(req, res) {
+  let owners = realm.objects('Owner').sorted('name', true);
+  res.render('pages/owners', {owners: owners});
+});
+
+let DBSchema = {
+  name: 'DB',
+  primaryKey: "name",
+  properties: {
+    timestamp: 'date',
+    name: 'string',
+    rating: 'string',
+    owner: {
+      type: 'linkingObjects',
+      objectType: 'Owner',
+      property: 'databases'
+    }
+  }
+};
+
+let OwnerSchema = {
+  name: 'Owner',
+  primaryKey: "name",
+  properties: {
+    name: 'string',
+    website: 'string',
+    databases: "DB[]"
+  }
+};
+
+var realm = new Realm({
+  path: 'ranking.realm',
+  schema: [DBSchema, OwnerSchema]
+})
+
+app.post('/add_db', function(req, res) {
+  let name = req.body['name'],
+    rating = req.body['rating'],
+    ownerName = req.body['owner'],
+    timestamp = new Date();
+
+    realm.write(() => {
+      const database = realm.create('DB', {name: name, rating: rating, timestamp: timestamp});
+      const ownerObject = realm.objectForPrimaryKey("Owner", ownerName);
+      ownerObject.databases.push(database);
+  });
+  res.redirect('/');
+});
+
+
+app.post('/add_owner', function(req, res) {
+  let name = req.body['name'],
+    url = req.body['url'];
+    realm.write(() => {
+      realm.create('Owner', {name: name, website: url, databases: []});
+  });
+  res.redirect('/');
 });
