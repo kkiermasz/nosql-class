b777e32d6a999ea42705f3792aeee717d0e0310f
Jakub Kiermasz <kiermasz.k@gmail.com>
2021-05-21 15:20:42 +0200

    Initial

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
 'use strict';
 
 var express = require('express');
-var bodyParser = require('body-parser');
+var Realm = require('realm');
 var app = express();
 
 app.use(express.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send("Hello Realm");
+  // TODO:
+  let databases = [];
+  res.render('pages/index', {databases: databases});
 });
 
 app.listen(3000, function() {
   console.log("Go!");
 });
 
-app.get('/add', function(req, res) {
-  res.sendFile(__dirname + "/add.html");
+app.get('/add_db', function(req, res) {
+  // TODO:
+  let owners = [];
+  res.render('pages/add_db', {owners: owners});
 });
 
-app.post('/add', function(req, res) {
-  res.send(req.body);
+app.get('/add_owner', function(req, res) {
+  res.render('pages/add_owner');
+});
+
+app.get('/owners', function(req, res) {
+  // TODO:
+  let owners = [];
+  res.render('pages/owners', {owners: owners});
+});
+
+let DBSchema = {
+  name: 'DB',
+  primaryKey: "name",
+  properties: {
+    timestamp: 'date',
+    name: 'string',
+    rating: 'string'
+    // TODO:
+    // owner: 
+  }
+};
+
+let OwnerSchema = {
+  name: 'Owner',
+  primaryKey: "name",
+  properties: {
+    name: 'string',
+    website: 'string'
+    // TODO:
+    // databases: 
+  }
+};
+
+var realm = new Realm({
+  path: 'ranking.realm',
+  schema: [DBSchema, OwnerSchema]
+})
+
+app.post('/add_db', function(req, res) {
+  // TODO
+  // add database
+  res.redirect('/');
+});
+
+
+app.post('/add_owner', function(req, res) {
+  // TODO
+  // add owner
+  res.redirect('/');
 });
